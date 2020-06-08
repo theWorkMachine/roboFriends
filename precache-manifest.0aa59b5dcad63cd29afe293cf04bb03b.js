60a01733ac57b73f8c6e2a934507cdc66529ec1e
TheWorkMachine <54051905+sobaanumer@users.noreply.github.com>
2020-06-08 11:25:33 +1000

    Updates

---
+++ precache-manifest.0aa59b5dcad63cd29afe293cf04bb03b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7073ddd593890425ee4b038f2385b78f",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/roboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "94d1d9ec240ee9a222e9",
+    "url": "/roboFriends/static/css/main.f0af73d5.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/roboFriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboFriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d1d9ec240ee9a222e9",
+    "url": "/roboFriends/static/js/main.94506464.chunk.js"
+  },
+  {
+    "revision": "db7f08724344aac129ee",
+    "url": "/roboFriends/static/js/runtime-main.ed696bc5.js"
+  },
+  {
+    "revision": "751a0e4b9f683e9539a39ff766070073",
+    "url": "/roboFriends/static/media/RockFont.751a0e4b.woff"
+  }
+]);
